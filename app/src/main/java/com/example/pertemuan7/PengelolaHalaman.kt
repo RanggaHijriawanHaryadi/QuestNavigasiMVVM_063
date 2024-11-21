eeb43685f6cb45416948ad0700bd6c17b7f403b6
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-11-21 20:32:54 +0700

    menambahkan modifier pada PengelolaHalaman

--- app/src/main/java/com/example/pertemuan7/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan7/PengelolaHalaman.kt
@@ -1,11 +1,13 @@
 package com.example.pertemuan7
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan7.ui.viewmodel.MahasiswaViewModel
 
 enum class  Halaman {
     Formulir,
@@ -13,8 +15,9 @@ enum class  Halaman {
 }
 @Composable
 fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
     navController: NavController = rememberNavController(),
-    viewModel: MahasiswaViewModel = viewModel()
+    viewmodel: MahasiswaViewModel = viewModel()
 ){
     val stateUi by view
     NavHost(navController = navController, startDestination = Halaman.Formulir.name) {
